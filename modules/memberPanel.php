8f1888155dd8699761c4ee21cf93285821005581
Kyle Casingal <you@example.com>
2022-11-21 15:15:31 +0800

    css fix

--- modules/memberPanel.php
+++ modules/memberPanel.php
@@ -33,7 +33,7 @@
   .memberSideBar {
     display: inline;
     justify-content: flex-end;
-    margin-top: 5px;
+    margin-top: 0.5vw;
     margin-bottom: 0;
     padding: 0;
     list-style: none;
