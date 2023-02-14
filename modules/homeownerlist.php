b8b2cebf3c6927b07a0a526a3e05b96ce7d7fac9
JeunePaolus <floresjeunepaolus@gmail.com>
2023-02-14 18:25:07 +0800

    Co-authored-by: MonCarloDelima <MonCarloDelima@users.noreply.github.com> Co-authored-by: Kyle Casingal <KyleCasingal@users.noreply.github.com>

---
+++ modules/homeownerlist.php
@@ -0,0 +1,481 @@
+<?php
+require '../marginals/topbar.php';
+$res = $con->query("SELECT * FROM user, homeowner_profile  WHERE user_id = " . $user_id = $_SESSION['user_id'] . "  AND full_name = CONCAT(first_name, ' ', last_name)") or die($mysqli->error);
+$row = $res->fetch_assoc();
+$result = $con->query("SELECT * FROM homeowner_profile WHERE email_address != '' ORDER BY homeowner_id ASC ") or die($mysqli->error);
+$resultSubd = $con->query("SELECT * FROM subdivision ORDER BY subdivision_id ASC") or die($mysqli->error);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="theme-color" content="#000000" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Newsreader:opsz@6..72&family=Poppins:wght@400;800&family=Special+Elite&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>SUNNYVALE</title>
+</head>
+<style>
+    * {
+        margin: 0;
+        font-family: 'Poppins', sans-serif;
+    }
+
+    input {
+        font-size: 1vw;
+        border-radius: 0.5vw;
+        border: none !important;
+
+    }
+
+    select {
+        font-size: 1vw;
+        border: none !important;
+        border-radius: 1vw;
+        width: 15vw;
+    }
+
+    .tblForm {
+        border: 0 none !important;
+
+    }
+
+    .tblForm th,
+    td {
+        font-size: 1vw;
+        padding: 1vw;
+    }
+
+    .secretary {
+        display: flex;
+    }
+
+    .sideBar {
+        background-color: rgb(248, 245, 227);
+        flex: 2;
+        color: black;
+    }
+
+    .secretarySideBar {
+        display: inline;
+        justify-content: flex-end;
+        margin-top: 5px;
+        margin-bottom: 0;
+        padding: 0;
+        list-style: none;
+    }
+
+    .secretarySideBar li {
+        color: rgb(89, 89, 89);
+        font-family: "Poppins", sans-serif;
+        text-align: center;
+        padding: 1.5vw;
+        padding-left: 0.5vw;
+        padding-right: 0.5vw;
+        font-size: max(1.5vw, min(10px));
+        cursor: pointer;
+        border-bottom: 1px solid lightgray;
+    }
+
+    .secretarySideBar li:hover {
+        background-color: rgb(236, 235, 226);
+    }
+
+    .secretaryPanel {
+        flex: 8;
+        width: 100%;
+        overflow-x: hidden;
+    }
+
+    .regForm {
+        background-color: rgb(170, 192, 175, 0.3);
+        width: 90%;
+        padding: 2vw;
+        margin: 2vw;
+        border-radius: 1vw;
+    }
+
+    .btnSubmitReg {
+        background-color: darkseagreen;
+        border: 0;
+        padding: 0.5vw;
+        max-width: 50vw;
+        width: 10vw;
+        font-family: "Poppins", sans-sans-serif;
+        font-size: 1vw;
+        margin-top: 2vw;
+        color: white;
+        border-radius: 0.8vw;
+        cursor: pointer;
+    }
+
+    .btnSubmitReg:hover {
+        background-color: rgb(93, 151, 93);
+    }
+
+    .btnUpdateReg {
+        background-color: orange;
+        border: 0;
+        padding: 0.5vw;
+        max-width: 50vw;
+        width: 10vw;
+        font-family: "Poppins", sans-sans-serif;
+        font-size: 1vw;
+        margin-top: 2vw;
+        color: white;
+        border-radius: 0.8vw;
+        cursor: pointer;
+    }
+
+    .btnUpdateReg:hover {
+        background-color: orangered;
+    }
+
+    .btnClearReg {
+        background-color: lightcoral;
+        border: 0;
+        padding: 0.5vw;
+        max-width: 50vw;
+        width: 10vw;
+        font-family: "Poppins", sans-sans-serif;
+        font-size: 1vw;
+        margin-top: 2vw;
+        color: white;
+        border-radius: 0.8vw;
+        cursor: pointer;
+    }
+
+    .btnClearReg:hover {
+        background-color: rgb(180, 83, 83);
+    }
+
+    .lblRegistration {
+        font-size: 2vw;
+        font-family: "Poppins", sans-serif;
+        margin-top: 1vw;
+        margin-left: 2vw;
+        margin-bottom: -2vw;
+        padding: 0;
+        color: rgb(89, 89, 89);
+        font-weight: 800;
+    }
+
+    .tblContainer {
+        justify-content: center;
+        margin-top: 0vw;
+        margin-bottom: 2vw;
+        margin-left: 2vw;
+        margin-right: 0;
+        overflow-y: auto;
+        overflow-x: auto;
+        /* max-height: 20vw; */
+    }
+
+    .tblHomeowners {
+        margin-bottom: 2vw;
+        overflow-x: auto;
+        overflow-y: auto;
+        text-align: center;
+        margin: 2vw;
+        margin-right: 2vw;
+        max-width: 100%;
+    }
+
+    .tblHomeowners thead,
+    th {
+        padding: 0.5vw;
+        text-align: center;
+        font-size: 1.2vw;
+        background-color: rgb(170, 192, 175, 0.3);
+        width: max-content;
+        white-space: nowrap;
+    }
+
+    .tblHomeowners td {
+        width: max-content;
+        white-space: nowrap;
+    }
+
+    .tblHomeowners tr:hover {
+        background-color: rgb(211, 211, 211);
+    }
+
+    .userManagementForm {
+        background-color: rgb(170, 192, 175, 0.3);
+        width: 95%;
+        padding: 2vw;
+        margin: 2vw;
+        border-radius: 1vw;
+    }
+
+    .tblUsers {
+        max-width: 95%;
+        margin-top: 2vw;
+    }
+
+    .tblUsers th {
+        text-align: center;
+    }
+
+    .tblUsers td {
+        text-align: center;
+    }
+
+    .btnArea {
+        display: flex;
+        margin: 2vw;
+        gap: 1vw;
+    }
+
+    .tbl tr:hover {
+        background-color: rgb(211, 211, 211);
+    }
+
+
+    /* complaint container */
+    .inbox {
+        overflow: hidden;
+        margin: 2vw;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .inboxContainer {
+        padding: 2vw;
+        padding-left: 0;
+        padding-right: 0;
+        border-radius: 1vw;
+        background-color: rgb(241, 241, 241);
+        display: flex;
+        width: 95%;
+        height: 70vh;
+        overflow-x: hidden;
+        overflow-y: scroll;
+    }
+
+    .tblMessage {
+        margin: 0;
+        width: 100%;
+        height: 100%;
+        max-height: 30px;
+    }
+
+    .trInbox {
+        width: 100%;
+        color: rgb(89, 89, 89);
+        background-color: rgb(241, 241, 241);
+        border-bottom: 1px solid rgb(192, 192, 192);
+    }
+
+    .trInbox:hover {
+        background-color: rgb(233, 233, 233);
+        cursor: pointer;
+    }
+
+    .msgSender {
+        font-family: "Poppins", sans-serif;
+        font-size: 1.5vw;
+        font-weight: bold;
+    }
+
+    .msgDesc {
+        max-width: 50vw;
+        width: fixed;
+        text-align: left;
+        font-family: "Poppins", sans-serif;
+        font-size: 1.2vw;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
+
+    .msgTime {
+        text-align: center;
+        font-family: "Poppins", sans-serif;
+        font-size: 1.2vw;
+    }
+
+    .inboxTitle {
+        font-size: 2vw;
+        font-family: "Poppins", sans-serif;
+        padding: 0;
+        margin-bottom: 1vw;
+        color: rgb(89, 89, 89);
+        font-weight: 800;
+    }
+
+    .complaintManagement {
+        margin: 2vw;
+    }
+
+    .lblNA {
+        margin: 0;
+        color: gray;
+        font-size: 0.8vw;
+        text-align: center;
+        font-style: italic;
+    }
+
+    .NA {
+        vertical-align: bottom !important;
+        text-align: center;
+        padding-bottom: 0 !important;
+    }
+
+    .NAemployer {
+        margin: 0;
+        padding-top: 2vw;
+    }
+
+    .btnEdit {
+        background-color: orange;
+        border: 0;
+        padding: 0.5vw;
+        max-width: 50vw;
+        width: 5vw;
+        font-family: "Poppins", sans-sans-serif;
+        font-size: .8vw;
+        margin-top: 2vw;
+        border-radius: 0.8vw;
+        cursor: pointer;
+        text-decoration: none;
+        color: white;
+    }
+
+    .buttonGrpRegister {
+        display: flex;
+        justify-content: end;
+        gap: 1em;
+    }
+
+    .btnImportReg {
+        background-color: orange;
+        border: 0;
+        padding: 0.5vw;
+        max-width: 50vw;
+        width: 10vw;
+        font-family: "Poppins", sans-sans-serif;
+        font-size: 1vw;
+        margin-top: 2vw;
+        color: white;
+        border-radius: 0.8vw;
+        cursor: pointer;
+    }
+</style>
+<script>
+    $("#form_id").trigger("reset");
+</script>
+
+<body>
+    <form method="post">
+        <div class="secretary">
+            <div class="sideBar">
+                <?php
+                if ($row['user_type'] == 'Admin') {
+                    require '../marginals/sidebarAdmin.php';
+                }
+
+                if ($row['user_type'] == 'Secretary') {
+                    require '../marginals/sidebarSecretaryPanel.php';
+                }
+                ?>
+            </div>
+
+            <div class="secretaryPanel">
+                <label class="lblRegistration">Registered Homeowners</label>
+                <div class="tblContainer">
+                    <table class="tblHomeowners">
+                        <thead>
+                            <th></th>
+                            <th>Homeowner ID</th>
+                            <th>Full Name</th>
+                            <th>Date of Birth</th>
+                            <th>Sex</th>
+                            <th>Residence Address</th>
+                            <th>Email</th>
+                            <th>Mobile Number</th>
+                            <th>Business Address</th>
+                            <th>Occupation</th>
+                            <th>Employer</th>
+                        </thead>
+                        <?php
+                        while ($row = $result->fetch_assoc()):
+                            $suffix = $row['suffix'];
+                            if ($suffix == "N/A") {
+                                $suffix = NULL;
+                            } else {
+                                $suffix = " " . $row['suffix'];
+                            }
+                            $middle_name = $row['middle_name'];
+                            if ($middle_name == "N/A") {
+                                $middle_name = NULL;
+                            } else {
+                                $middle_name = " " . $row['middle_name'];
+                            }
+                            $residence_address = $row['street'] . ' ' . $row['subdivision'] . ' ' . $row['barangay'];
+
+                            ?>
+                            <tr>
+                                <td>
+                                    <a href="homeownerRegistration.php?homeowner_id=<?php echo $row['homeowner_id']; ?>"
+                                        class="btnEdit">Edit</a>
+                                </td>
+                                <td>
+                                    <?php echo $row['homeowner_id']; ?>
+                                </td>
+                                <td>
+                                    <?php echo $row['first_name'] . $middle_name . " " . $row['last_name'] . $suffix; ?>
+                                </td>
+                                <td>
+                                    <?php
+                                    $datetime = strtotime($row['birthdate']);
+                                    echo $phptime = date("m/d/Y", $datetime);
+                                    ?>
+                                </td>
+                                <td>
+                                    <?php echo $row['sex']; ?>
+                                </td>
+                                <td>
+                                    <?php echo $residence_address; ?>
+                                </td>
+                                <td>
+                                    <?php echo $row['email_address']; ?>
+                                </td>
+                                <td>
+                                    <?php echo $row['mobile_number']; ?>
+                                </td>
+                                <td>
+                                    <?php echo $row['business_address']; ?>
+                                </td>
+                                <td>
+                                    <?php echo $row['occupation']; ?>
+                                </td>
+                                <td>
+                                    <?php echo $row['employer']; ?>
+                                </td>
+                            </tr>
+                        <?php endwhile; ?>
+                    </table>
+                </div>
+            </div>
+        </div>
+        </div>
+
+    </form>
+    <?php
+    require '../marginals/footer2.php'
+        ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
