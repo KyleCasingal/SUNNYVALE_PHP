e3130b0d2e704e1aab4b800f17892ca2cc906ce3
JeunePaolus <floresjeunepaolus@gmail.com>
2023-03-12 16:18:50 +0800

    remove calendar

--- modules/amenities.php
+++ modules/amenities.php
@@ -177,7 +177,7 @@ $resultTotal = $con->query("SELECT SUM(cost) AS total_cost FROM amenity_renting 
     border-radius: 0.8vw;
     cursor: pointer;
   }
-  .calendarshow{
+  /* .calendarshow{
     background-color: darkseagreen;
     border: 0;
     padding: 0.5vw;
@@ -191,7 +191,7 @@ $resultTotal = $con->query("SELECT SUM(cost) AS total_cost FROM amenity_renting 
     cursor: pointer;
     
   
-  }
+  } */
 
   .btnCompute {
     background-color: rgb(248, 186, 55);
@@ -246,7 +246,7 @@ $resultTotal = $con->query("SELECT SUM(cost) AS total_cost FROM amenity_renting 
 
   .tblAmenity thead,
   th {
-    padding: 0.5vw;
+    /* padding: 0.5vw; */
     text-align: center;
     font-size: 1.2vw;
     background-color: rgb(170, 192, 175, 0.3);
@@ -262,7 +262,9 @@ $resultTotal = $con->query("SELECT SUM(cost) AS total_cost FROM amenity_renting 
   .tblAmenity tr:hover {
     background-color: rgb(211, 211, 211);
   }
-
+#fullcalendar {
+justify-content: center;
+}
  
 </style>
 <script type="text/javascript">
@@ -588,9 +590,9 @@ $resultTotal = $con->query("SELECT SUM(cost) AS total_cost FROM amenity_renting 
         </div>
       </div>
     </div>
-
+    <!-- <div class='amenitiesForm' id=fullcalendar>
     <div id='calendar'></div>
-  
+    </div> -->
     </div>
   </form>
   <?php
@@ -630,85 +632,87 @@ $resultTotal = $con->query("SELECT SUM(cost) AS total_cost FROM amenity_renting 
   $(document).ready(function() {
 	display_events();
 });
-function display_events() {
-	var events = new Array();
-$.ajax({
-    url: 'display_event.php',  
-    dataType: 'json',
-    success: function (response) {
-         
-    var result=response.data;
-    $.each(result, function (i, item) {
-    	events.push({
-            event_id: result[i].event_id,
-            title: result[i].title,
-            start: result[i].start,
-            end: result[i].end,
-            color: result[i].color,
-            url: result[i].url
-        }); 	
-    })
-	var calendar = $('#calendar').fullCalendar({
-	    defaultView: 'month',
-		 timeZone: 'local',
-	    editable: true,
-        selectable: true,
-		selectHelper: true,
-        select: function(start, end) {
-				alert(start);
-				alert(end);
-				$('#event_start_date').val(moment(start).format('YYYY-MM-DD'));
-				$('#event_end_date').val(moment(end).format('YYYY-MM-DD'));
-				$('#event_entry_modal').modal('show');
-			},
-        events: events,
-	    eventRender: function(event, element, view) { 
-            element.bind('click', function() {
-					alert(event.event_id);
-				});
-    	}
-		}); //end fullCalendar block	
-	  },//end success block
-	  error: function (xhr, status) {
-	  alert(response.msg);
-	  }
-	});//end ajax block	
-}
 
-function save_event()
-{
-var event_name=$("#event_name").val();
-var event_start_date=$("#event_start_date").val();
-var event_end_date=$("#event_end_date").val();
-if(event_name=="" || event_start_date=="" || event_end_date=="")
-{
-alert("Please enter all required details.");
-return false;
-}
-$.ajax({
- url:"save_event.php",
- type:"POST",
- dataType: 'json',
- data: {event_name:event_name,event_start_date:event_start_date,event_end_date:event_end_date},
- success:function(response){
-   $('#event_entry_modal').modal('hide');  
-   if(response.status == true)
-   {
-	alert(response.msg);
-	location.reload();
-   }
-   else
-   {
-	 alert(response.msg);
-   }
-  },
-  error: function (xhr, status) {
-  console.log('ajax error = ' + xhr.statusText);
-  alert(response.msg);
-  }
-});    
-return false;
-}
+// //calendar
+// function display_events() {
+// 	var events = new Array();
+// $.ajax({
+//     url: 'display_event.php',  
+//     dataType: 'json',
+//     success: function (response) {
+         
+//     var result=response.data;
+//     $.each(result, function (i, item) {
+//     	events.push({
+//             event_id: result[i].event_id,
+//             title: result[i].title,
+//             start: result[i].start,
+//             end: result[i].end,
+//             color: result[i].color,
+//             url: result[i].url
+//         }); 	
+//     })
+// 	var calendar = $('#calendar').fullCalendar({
+// 	    defaultView: 'month',
+// 		 timeZone: 'local',
+// 	    editable: true,
+//         selectable: true,
+// 		selectHelper: true,
+//         select: function(start, end) {
+// 				alert(start);
+// 				alert(end);
+// 				$('#event_start_date').val(moment(start).format('YYYY-MM-DD'));
+// 				$('#event_end_date').val(moment(end).format('YYYY-MM-DD'));
+// 				$('#event_entry_modal').modal('show');
+// 			},
+//         events: events,
+// 	    eventRender: function(event, element, view) { 
+//             element.bind('click', function() {
+// 					alert(event.event_id);
+// 				});
+//     	}
+// 		}); //end fullCalendar block	
+// 	  },//end success block
+// 	  error: function (xhr, status) {
+// 	  alert(response.msg);
+// 	  }
+// 	});//end ajax block	
+// }
+
+// function save_event()
+// {
+// var event_name=$("#event_name").val();
+// var event_start_date=$("#event_start_date").val();
+// var event_end_date=$("#event_end_date").val();
+// if(event_name=="" || event_start_date=="" || event_end_date=="")
+// {
+// alert("Please enter all required details.");
+// return false;
+// }
+// $.ajax({
+//  url:"save_event.php",
+//  type:"POST",
+//  dataType: 'json',
+//  data: {event_name:event_name,event_start_date:event_start_date,event_end_date:event_end_date},
+//  success:function(response){
+//    $('#event_entry_modal').modal('hide');  
+//    if(response.status == true)
+//    {
+// 	alert(response.msg);
+// 	location.reload();
+//    }
+//    else
+//    {
+// 	 alert(response.msg);
+//    }
+//   },
+//   error: function (xhr, status) {
+//   console.log('ajax error = ' + xhr.statusText);
+//   alert(response.msg);
+//   }
+// });    
+// return false;
+// }
 </script>
 
 </html>
