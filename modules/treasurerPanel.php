9fb261c46546d3c08751d814a46279f6ca846924
MonCarloDelima <dmoncarlo6@gmail.com>
2023-04-09 16:31:29 +0800

    fixes

--- modules/treasurerPanel.php
+++ modules/treasurerPanel.php
@@ -184,6 +184,7 @@ $resultBilling = $con->query(
     background-color: rgb(170, 192, 175);
 
   }
+  
 </style>
 
 <body>
@@ -286,5 +287,6 @@ $resultBilling = $con->query(
     }
   };
 </script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 
 </html>
