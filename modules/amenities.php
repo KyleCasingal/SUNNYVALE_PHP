8e493b0ee8f139dce198620e1d575b845c51560e
JeunePaolus <floresjeunepaolus@gmail.com>
2023-05-04 23:22:21 +0800

    calendar

--- modules/amenities.php
+++ modules/amenities.php
@@ -39,7 +39,13 @@ $resultTotal = $con->query("SELECT SUM(cost) AS total_cost FROM amenity_renting 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.js"></script>
 
 
-  
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="css/calendar.css">
+
+
   <!-- bootstrap css and js -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />
   <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script> -->
@@ -543,6 +549,35 @@ $resultTotal = $con->query("SELECT SUM(cost) AS total_cost FROM amenity_renting 
 </script>
 
 <body>
+  <div class="container">
+    <div class="page-header">
+      <div class="pull-right form-inline">
+        <div class="btn-group">
+          <button class="btn btn-primary" data-calendar-nav="prev">
+            << Prev</button>
+              <button class="btn btn-default" data-calendar-nav="today">Today</button>
+              <button class="btn btn-primary" data-calendar-nav="next">Next >></button>
+        </div>
+        <div class="btn-group">
+          <button class="btn btn-warning" data-calendar-view="year">Year</button>
+          <button class="btn btn-warning active" data-calendar-view="month">Month</button>
+          <button class="btn btn-warning" data-calendar-view="week">Week</button>
+          <button class="btn btn-warning" data-calendar-view="day">Day</button>
+        </div>
+      </div>
+      <h3></h3>
+    </div>
+    <div class="row">
+      <div class="col-md-9">
+        <div id="showEventCalendar"></div>
+      </div>
+      <div class="col-md-3">
+        <h4>All Events List</h4>
+        <ul id="eventlist" class="nav nav-list"></ul>
+      </div>
+    </div>
+  </div>
+
   <form method="post" enctype="multipart/form-data">
 
     <div class="fab-wrapper">
@@ -576,7 +611,7 @@ $resultTotal = $con->query("SELECT SUM(cost) AS total_cost FROM amenity_renting 
           </div>
           <div class="modal-body get_quote_view_modal_body">
             <div class="calendar-container">
-              <div id="calendar" class ="calendar"></div>
+              <div id="calendar" class="calendar"></div>
             </div>
           </div>
         </div>
@@ -781,7 +816,11 @@ $resultTotal = $con->query("SELECT SUM(cost) AS total_cost FROM amenity_renting 
   require '../marginals/footer2.php';
   ?>
 
+
 </body>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js"></script>
+<script type="text/javascript" src="js/calendar.js"></script>
+<script type="text/javascript" src="js/events.js"></script>
 <script>
   $('#select-all').click(function(event) {
     if (this.checked) {
