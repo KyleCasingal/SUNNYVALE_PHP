a5344d83fbe8f465b43d84e6fa75f70848f2b3f7
JeunePaolus <floresjeunepaolus@gmail.com>
2023-05-08 16:33:36 +0800

    vehicle bike

--- modules/vehicleSticker.php
+++ modules/vehicleSticker.php
@@ -11,8 +11,12 @@ if ($_SESSION['subdivision'] != '') {
 }
 $resultTransaction1 = $con->query("SELECT * FROM transaction WHERE transaction_type = 'Vehicle Sticker'");
 $resultTransaction2 = $con->query("SELECT * FROM transaction WHERE transaction_type = 'Vehicle Sticker'");
-$result0 = $con->query("SELECT * FROM subdivision ORDER BY subdivision_id ASC");
 
+if ($_SESSION['subdivision'] != '') {
+    $result0 = $con->query("SELECT * FROM subdivision WHERE subdivision_name = '" .  $_SESSION['subdivision'] . "' ORDER BY subdivision_id ASC ");
+} else {
+    $result0 = $con->query("SELECT * FROM subdivision ORDER BY subdivision_id ASC");
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -639,6 +643,9 @@ $result0 = $con->query("SELECT * FROM subdivision ORDER BY subdivision_id ASC");
     </div>
 
 
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
     <script>
         document.getElementById("quantity").addEventListener("keydown", e => e.keyCode != 38 && e.keyCode != 40 && e.preventDefault());
