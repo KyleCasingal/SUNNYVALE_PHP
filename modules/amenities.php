3841a4dcf2008fb78a2bea7b704cba6f96769975
MonCarloDelima <dmoncarlo6@gmail.com>
2023-05-09 15:22:11 +0800

    fixes

--- modules/amenities.php
+++ modules/amenities.php
@@ -783,6 +783,22 @@ $resultTotal = $con->query("SELECT SUM(cost) AS total_cost FROM amenity_renting 
         });
       }
     });
+
+    function readURL(input, id) {
+      if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+          $('#' + id).attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+      }
+    }
+
+    $("#image").change(function() {
+      readURL(this, 'imagePreview');
+    });
   </script>
   <?php
   require '../marginals/footer2.php';
